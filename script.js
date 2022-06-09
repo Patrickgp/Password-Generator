bb7d40684ccdbc44b6f7304a3df39e9cbf988164
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-06-09 13:41:50 -0400

    created functions that will confirm or deny character types via window prompts

--- script.js
+++ script.js
@@ -1,99 +1,48 @@
 // Assignment code here
 var password = document.getElementById("password");
-// Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
 
-var passwordLength = 20;
-var password = "";
-
-for (var i = 0; i <= passwordLength; i++) {
-  var random 
+// functions below initiate prompts confirm or denying the types of characters used in the password
+function confirmLowerCase() {
+  let confirmLowerCase = confirm("Did you want to include lower case letters?");
+  if (confirmLowerCase) {
+    alert("Lowercase lettering will be included in your password")
+  } else alert("Lowercase lettering will NOT be included in your password")
 }
 
+function confirmUpperCase() {
+  let confirmUpperCase = confirm("Did you want to include upper case letters?");
+  if (confirmUpperCase) {
+  alert("Uppercase lettering will be included in your password")
+  } else alert("Uppercase lettering will NOT be included in your password")
+}
+
+function confirmSpecialCharacters() {
+  let confirmSpecialCharacters = confirm("Did you want to include special characters?");
+  if (confirmSpecialCharacters) {
+  alert("Special characters will be included in your password")
+  } else alert("Special characters will NOT be included in your password")
+}
+
+function confirmNumbers() {
+  let confirmNumbers = confirm("Did you want to include numbers?");
+  if (confirmNumbers) {
+  alert("Numbers will be included in your password")
+  } else alert("Numbers will NOT be included in your password")
+}
+
+function passwordLength() {
+ var confirmlength = parseInt(window.prompt("Password Length: Please enter a number between 1 - 128."));
+  while (confirmlength > 128 || confirmlength < 1) {
+    window.alert ("You need to provide a valid integer. Please try again.");
+    return passwordLength();
+  }
+}
+
+confirmLowerCase();
+confirmUpperCase();
+confirmSpecialCharacters();
+confirmNumbers();
+passwordLength();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
