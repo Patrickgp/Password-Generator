1355f661527cbdb922d8689b0e4cbadbfbd74111
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-06-09 14:01:57 -0400

    working on creating a function that parses through confirmed or denied criteria and includes them in the password generation

--- script.js
+++ script.js
@@ -1,29 +1,115 @@
 // Assignment code here
 var password = document.getElementById("password");
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
 
 // functions below initiate prompts confirm or denying the types of characters used in the password
-function confirmLowerCase() {
+function confirmLowerCase(lowerCasedCharacters) {
   let confirmLowerCase = confirm("Did you want to include lower case letters?");
   if (confirmLowerCase) {
     alert("Lowercase lettering will be included in your password")
   } else alert("Lowercase lettering will NOT be included in your password")
 }
 
-function confirmUpperCase() {
+function confirmUpperCase(upperCasedCharacters) {
   let confirmUpperCase = confirm("Did you want to include upper case letters?");
   if (confirmUpperCase) {
   alert("Uppercase lettering will be included in your password")
   } else alert("Uppercase lettering will NOT be included in your password")
 }
 
-function confirmSpecialCharacters() {
+function confirmSpecialCharacters(specialCharacters) {
   let confirmSpecialCharacters = confirm("Did you want to include special characters?");
   if (confirmSpecialCharacters) {
   alert("Special characters will be included in your password")
   } else alert("Special characters will NOT be included in your password")
 }
 
-function confirmNumbers() {
+function confirmNumbers(numericCharacters) {
   let confirmNumbers = confirm("Did you want to include numbers?");
   if (confirmNumbers) {
   alert("Numbers will be included in your password")
@@ -53,7 +139,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
